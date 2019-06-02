6c475d899bbd23820d792c30d5c59454b7a885ed
sAMs126 <492457689@qq.com>
2019-06-02 12:52:03 +0800

    新增功能点：获取访问地址ip

--- src/main/java/com/software/pm/common/aspect/WebLogAspect.java
+++ src/main/java/com/software/pm/common/aspect/WebLogAspect.java
@@ -1,5 +1,6 @@
 package com.software.pm.common.aspect;
 
+import com.software.pm.utils.IPUtils;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
@@ -33,7 +34,7 @@ public class WebLogAspect {
         logger.info("请求地址 : " + request.getRequestURL().toString());
         logger.info("HTTP METHOD : " + request.getMethod());
         // 获取真实的ip地址
-        //logger.info("IP : " + IPAddressUtil.getClientIpAddress(request));
+        logger.info("IP : " + IPUtils.getIpAddr(request));
         logger.info("CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "."
                 + joinPoint.getSignature().getName());
         logger.info("参数 : " + Arrays.toString(joinPoint.getArgs()));
